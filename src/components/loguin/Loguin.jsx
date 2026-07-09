e176758390ac4bfbf300b61da5ced8aa9cb97fbe
Yessica <yessilpd91@outlook.com>
2026-07-08 22:44:44 -0300

    arreglando css

--- src/components/loguin/Loguin.jsx
+++ src/components/loguin/Loguin.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useAuth } from "../../context/AuthContext"
-import { useNavigate } from "react-router-dom";
-import "./loguin.css";
+import { useNavigate } from "react-router-dom"
+import './loguin.css'
 
 export const Login = () => {
     const {login} = useAuth()
