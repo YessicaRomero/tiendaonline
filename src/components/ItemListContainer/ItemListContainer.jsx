a835634914ab8a38018ed299eb959092e6d80b5d
Yessica <yessilpd91@outlook.com>
2026-07-09 15:44:48 -0300

    Corrige import de ItemList

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from "react";
-
+import ItemList from "../ItemList/ItemList";
 
 import { useParams } from "react-router-dom";
 import { getByCategory } from "../../services/productService";
-import { ItemList } from "../itemList/itemList"; 
+
 
 
 
